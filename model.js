22532a77ee0740a2317f2ea8d3b14c521aec56ac
burhan <burhan@burhan-Aspire-6935.(none)>
2013-04-12 17:25:04 +0200

    Added loadingTemplate for ItemView.

--- model.js
+++ model.js
@@ -1,35 +1,94 @@
-/**
- * Add default functionality such as sync all changes except ones defined in
- * modifyExcluded.
- */
-define(['backbone', 'cocktail'], function(Backbone) {
+// Crux model.
+//
+// Add default functionality.
+// 
+// http://www.codeyellow.nl
+//
+// April 2013, AB Zainuddin
+define(['backbone', 'helper/type.of'], function(Backbone, type) {
     return Backbone.Model.extend({
+        _isFetching: false,
         /**
-         * Do not sync attributes defined in modifyExcluded.
+         * Returns true if attributes == defaults (excluding id).
+         * 
+         * @return bool True if empty
          */
-        modifyExcluded: ['id', 'created_at', 'updated_at'],
-        initialize: function() {
-            // Sync all changes (excluding ones defined in modifyExcluded.
-            this.on('change', this.modify);
+        isEmpty: function() {
+            return !_.isEqual(this.defaults, this.toJSON());
         },
-        /**
-         * Save all modifications except ones definded in modifyExcluded and
-         * properties that are '_unpurified'.
-         */
-        modify: function() {
-            var diff = _.difference(_.keys(this.changedAttributes()), this.modifyExcluded);
-            var remaining = [];
+        fetch: function(options) {
+            var that = this,
+            
+            // Set data after succes.
+            clearIsFetching = function(response) {
+                that._isFetching = false;
+                that.trigger('after:fetch');
+            };
 
-            // Remove all _unpurified.
-            _.each(diff, function(val) {
-                val.indexOf('_unpurified') == -1 && remaining.push(val);
-            });
+            options = options || {};
+            
+            this._isFetching = true;
+            this.trigger('before:fetch');
 
-            console.log('remaining:', remaining);
-
-            if (remaining.length > 0) {
-                this.save();
+            // Add success handler.
+            switch (type.of(options.success)) {
+                case 'function':
+                    options.success = [options.success, clearIsFetching];
+                    break;
+                case 'array':
+                    options.success.push(clearIsFetching);
+                    break;
+                default:
+                    options.success = clearIsFetching;
+                    break;
             }
+
+            // Add error handler.
+            switch (type.of(options.error)) {
+                case 'function':
+                    options.error = [options.error, clearIsFetching];
+                    break;
+                case 'array':
+                    options.error.push(success);
+                    break;
+                default:
+                    options.error = clearIsFetching;
+                    break;
+            }
+
+            Backbone.Model.prototype.fetch.call(this, options);
+        },
+        isFetching: function() {
+            return this._isFetching;
         }
+
+
+        // /**
+        //  * Do not sync attributes defined in modifyExcluded.
+        //  */
+        // modifyExcluded: ['id', 'created_at', 'updated_at'],
+        // initialize: function() {
+        //     // Sync all changes (excluding ones defined in modifyExcluded.
+        //     this.on('change', this.modify);
+        // },
+        // /**
+        //  * Save all modifications except ones definded in modifyExcluded and
+        //  * properties that are '_unpurified'.
+        //  */
+        // modify: function() {
+        //     var diff = _.difference(_.keys(this.changedAttributes()), this.modifyExcluded);
+        //     var remaining = [];
+
+        //     // Remove all _unpurified.
+        //     _.each(diff, function(val) {
+        //         val.indexOf('_unpurified') == -1 && remaining.push(val);
+        //     });
+
+        //     console.log('remaining:', remaining);
+
+        //     if (remaining.length > 0) {
+        //         this.save();
+        //     }
+        // }
     });
 });
--- view.js
+++ view.js
@@ -1,83 +1,106 @@
-/**
- * Add default functionality such as assigning the target element for view
- * rendering.
- */
-define(['backbone', 'helper/type.of', 'cocktail'], function(Backbone, type) {
-    return Backbone.View.extend({
-        initialize: function(options){
-//            if(type.of(window.views) == 'undefined') {
-//                window.views = [];
-//            }
-//            
-//            window.views.push(this);
-            this.subviews = [];
-            
-            Backbone.View.prototype.initialize.call(this, options);
-            
-//            console.log('created ' + this.cid);
-        },
-        /**
-         * Assign target $el where render should occure. Useful for subviews.
-         * 
-         * @link http://ianstormtaylor.com/rendering-views-in-backbonejs-isnt-always-simple/
-         */
-        assign: function(selector) {
-            var $el = null;
+// Crux model.
+//
+// Add default functionality.
+// 
+// http://www.codeyellow.nl
+//
+// April 2013, AB Zainuddin
+define(['marionette', 'cocktail', 'underscore'], function(Marionette, Cocktail, _) {
 
-            if (type.of(selector) == 'string') {
-                $el = this.$(selector);
-            } else {
-                $el = selector;
-            }
+    // Overwrite getTemplate to accomodate loadingTemplate.
+    var getTemplate = Marionette.View.prototype.getTemplate;
 
-            this.setElement($el);
-            
-            return this;
-        },
-        remove: function() {
-            this.trigger('beforeRemove ' + this.cid);
-            
-            this.removeSubviews();
-            this.undelegateEvents();
-
-            this.$el.removeData().unbind(); 
-
-            Backbone.View.prototype.remove.call(this);
-        },
-        subview: function() {
-            var subviews = this.subviews; 
-            
-            _.each(arguments, function(subview){
-                subviews.push(subview);   
-//                console.log('added subview ' + subview.cid);
-            });
-        },
-        removeSubviews: function() {
-            _.each(this.subviews, function(subview) {
-                subview.remove();
-            });
-            
-            this.subviews.length = 0;
-        },
-        countSubviews: function() {
-            var count = 0;
-            
-            _.each(this.subviews, function(subview) {
-               count += subview.countSubviews();
-               
-            });
-            
-            count += this.subviews.length;
-            
-            return count;
-        },
-        renderSubviews: function() {
-            _.each(this.subviews, function(subview) {
-                subview.render();
-             });
+    Marionette.View.prototype.getTemplate = function(options) {
+        switch(true) {
+            // Check if model is loading and if there is a loadingTemplate.
+            case this.model && this.model.isFetching && _.isFunction(this.model.isFetching) && this.model.isFetching() && Marionette.getOption(this, "loadingTemplate") != 'undefined':
+                return Marionette.getOption(this, "loadingTemplate");
+                break;
+            default:
+                return getTemplate.call(this, options);    
+                break;
         }
+    };
+
+    return Marionette;
+
+
+
+    // return Backbone.View.extend({
+//         initialize: function(options){
+// //            if(type.of(window.views) == 'undefined') {
+// //                window.views = [];
+// //            }
+// //            
+// //            window.views.push(this);
+//             this.subviews = [];
+            
+//             Backbone.View.prototype.initialize.call(this, options);
+            
+// //            console.log('created ' + this.cid);
+//         },
+//         /**
+//          * Assign target $el where render should occure. Useful for subviews.
+//          * 
+//          * @link http://ianstormtaylor.com/rendering-views-in-backbonejs-isnt-always-simple/
+//          */
+//         assign: function(selector) {
+//             var $el = null;
+
+//             if (type.of(selector) == 'string') {
+//                 $el = this.$(selector);
+//             } else {
+//                 $el = selector;
+//             }
+
+//             this.setElement($el);
+            
+//             return this;
+//         },
+//         remove: function() {
+//             this.trigger('beforeRemove ' + this.cid);
+            
+//             this.removeSubviews();
+//             this.undelegateEvents();
+
+//             this.$el.removeData().unbind(); 
+
+//             Backbone.View.prototype.remove.call(this);
+//         },
+//         subview: function() {
+//             var subviews = this.subviews; 
+            
+//             _.each(arguments, function(subview){
+//                 subviews.push(subview);   
+// //                console.log('added subview ' + subview.cid);
+//             });
+//         },
+//         removeSubviews: function() {
+//             _.each(this.subviews, function(subview) {
+//                 subview.remove();
+//             });
+            
+//             this.subviews.length = 0;
+//         },
+//         countSubviews: function() {
+//             var count = 0;
+            
+//             _.each(this.subviews, function(subview) {
+//                count += subview.countSubviews();
+               
+//             });
+            
+//             count += this.subviews.length;
+            
+//             return count;
+//         },
+//         renderSubviews: function() {
+//             _.each(this.subviews, function(subview) {
+//                 subview.render();
+//              });
+//         }
         
         
     
-    });
+    // });
 });
