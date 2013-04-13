eaba2cf274fc39bba2b5ae50a5604f822d2be5e0
burhan <burhan@burhan-Aspire-6935.(none)>
2013-04-13 22:26:32 +0200

    Fixed Paginator options.

--- collection.js
+++ collection.js
@@ -1,73 +1,59 @@
-/**
- * Add default functionality such as saving an entire collection.
- */
-define(['backbone.paginator', 'helper/type.of', 'helper/errorxhr', 'cocktail'], function(Paginator, type, errorXHR) {
+// Crux Collection
+// ---------------
+//
+// Add default functionality.
+// 
+// http://www.codeyellow.nl
+//
+// April 2013, AB Zainuddin
+define(['backbone.paginator'], function(Paginator) {
     return Paginator.requestPager.extend({
+        // Initialize
+        //-----------
         initialize: function(models, options) {
-            if (type.of(options) == 'undefined') {
-                options = {};
-            }
-
             options = $.extend(true, {
                 attributes: {}
-            }, options);
+            }, options || {});
 
-            /**
-             * Holds collection attributes. This will be added as data to each
-             * fetch.
-             */
+            // Config Paginator.
+            this.paginator_core = $.extend(true, {
+                type: 'GET',
+                dataType: 'json',
+                // Proxy Backbone.Collection url.
+                url: this.url
+            }, this.paginator_core || {});
+
+            this.paginator_ui = $.extend(true, {
+                firstPage: 0,
+                currentPage: 0,
+                perPage: 20,
+                totalPages: 10
+            }, this.paginator_ui || {});
+
+            this.server_api = $.extend(true, {
+                'limit': function() {
+                    return this.perPage
+                },
+                'offset': function() {
+                    return this.currentPage * this.perPage
+                }
+            }, this.server_api || {});
+
+
+            // Holds collection attributes. This will be added as data to each fetch.             
             this.attributes = new Backbone.Model(options.attributes);
 
+            // Keep track of collections' xhr. 
             this.xhr = null;
 
-            // Fetch on all changes.
-            // this.attributes.on('change', this.refresh, this);
-
             // Call parent.
-            Backbone.Collection.prototype.initialize.call(this, models, options);
-        },
-        paginator_core: {
-            // the type of the request (GET by default)
-            type: 'GET',
-
-            // the type of reply (jsonp by default)
-            dataType: 'json',
-
-            // the URL (or base URL) for the service
-            url: ''
-        },
-        paginator_ui: {
-            // the lowest page index your API allows to be accessed
-            firstPage: 0,
-
-            // which page should the paginator start from
-            // (also, the actual page the paginator is on)
-            currentPage: 0,
-
-            // how many items per page should be shown
-            perPage: 20,
-
-            // a default number of total pages to query in case the API or
-            // service you are using does not support providing the total
-            // number of pages for us.
-            // 10 as a default in case your service doesn't return the total
-            totalPages: 10
-        },
-        server_api: {
-            'limit': function() {
-                return this.perPage
-            },
-            'offset': function() {
-                return this.currentPage * this.perPage
-            }
+            Paginator.requestPager.prototype.initialize.call(this, models, options);
         },
         parse: function(response) {
             this.totalRecords = response.totalRecords;
             return response.data;
         },
-        /**
-         * Refresh data collection.
-         */
+        // Refresh data collection.
         refresh: function(params) {
             if (params) {
                 switch(true) {
@@ -85,16 +71,16 @@ define(['backbone.paginator', 'helper/type.of', 'helper/errorxhr', 'cocktail'], 
             // Set paging start to 0.
             this.currentPage = 0;
 
+            // Abort other xhr's;
             if (this.xhr) {
                 this.xhr.abort();
             }
 
-            this.xhr = this.fetch({
-                update: true
-            });
+            this.xhr = this.fetch(params);
 
             return this.xhr;
         },
+        // Save entire collection.
         save: function(options) {
             if (this.xhr) {
                 this.xhr.abort();
@@ -102,33 +88,23 @@ define(['backbone.paginator', 'helper/type.of', 'helper/errorxhr', 'cocktail'], 
 
             this.xhr = Backbone.sync('update', this, options);
         },
-        /**
-         * Override default fetch to add attributes to fetch.
-         * 
-         * @param object
-         *            options
-         */
+        // Override default fetch to add attributes.
         fetch: function(options) {
             var defaults = {
                 data: this.attributes.toJSON()
             };
 
-            var xhr = Backbone.Collection.prototype.fetch.call(this, _.extend(defaults, options));
+            this.xhr = Backbone.Collection.prototype.fetch.call(this, _.extend(defaults, options));
 
-            // Trigger fetch.
-            this.trigger('fetch', xhr);
-            console.log('triggered fetch');
-            
-            return xhr;
+            return this.xhr;
         },
-        /**
-         * Fetch 1 model by id from collection if exists, else from server.
-         * 
-         * Inspired by Stackoverflow.
-         * 
-         * @see http://stackoverflow.com/questions/6262444/get-collection-id-in-backbone-without-loading-the-entire-collection
-         * @return Crux/Model
-         */
+        // Fetch 1 model by id from collection if exists, else from server.
+        //
+        // Inspired by Stackoverflow.
+        // 
+        // `@see http://stackoverflow.com/questions/6262444/get-collection-id-in-backbone-without-loading-the-entire-collection`
+        // `@return Model`
+        //
         fetchOne: function(id, params) {
             var model = this.get(id);
             var params = params || {};
