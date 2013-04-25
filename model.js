0109168cf47a95edda65f8f11e6ef08efcad8c7b
burhan <burhan@burhan-Aspire-6935.(none)>
2013-04-25 09:54:52 +0200

    Fixed bug success/error handler. Backbone doesn't use jQuery success handlers which uses arrays. Now use promise.

--- model.js
+++ model.js
@@ -5,7 +5,7 @@
 // http://www.codeyellow.nl
 //
 // April 2013, AB Zainuddin
-define(['backbone', 'marionette', 'helper/type.of'], function(Backbone, Marionette, type) {
+define(['backbone', 'marionette', 'underscore', 'helper/type.of'], function(Backbone, Marionette, _, type) {
     return Backbone.Model.extend({
         _isFetching: false,
         /**
@@ -33,7 +33,10 @@ define(['backbone', 'marionette', 'helper/type.of'], function(Backbone, Marionet
             // Add success handler.
             switch (type.of(options.success)) {
                 case 'function':
-                    options.success = [options.success, clearIsFetching];
+                    options.success = _.wrap(options.success, function(func, model, resp, options){
+                        clearIsFetching();
+                        func(model, resp, options);
+                    });
                     break;
                 case 'array':
                     options.success.push(clearIsFetching);
@@ -46,7 +49,10 @@ define(['backbone', 'marionette', 'helper/type.of'], function(Backbone, Marionet
             // Add error handler.
             switch (type.of(options.error)) {
                 case 'function':
-                    options.error = [options.error, clearIsFetching];
+                    options.error = _.wrap(options.error, function(func, model, resp, options){
+                        clearIsFetching();
+                        func(model, resp, options);
+                    });
                     break;
                 case 'array':
                     options.error.push(success);
@@ -61,34 +67,5 @@ define(['backbone', 'marionette', 'helper/type.of'], function(Backbone, Marionet
         isFetching: function() {
             return this._isFetching;
         }
-
-
-        // /**
-        //  * Do not sync attributes defined in modifyExcluded.
-        //  */
-        // modifyExcluded: ['id', 'created_at', 'updated_at'],
-        // initialize: function() {
-        //     // Sync all changes (excluding ones defined in modifyExcluded.
-        //     this.on('change', this.modify);
-        // },
-        // /**
-        //  * Save all modifications except ones definded in modifyExcluded and
-        //  * properties that are '_unpurified'.
-        //  */
-        // modify: function() {
-        //     var diff = _.difference(_.keys(this.changedAttributes()), this.modifyExcluded);
-        //     var remaining = [];
-
-        //     // Remove all _unpurified.
-        //     _.each(diff, function(val) {
-        //         val.indexOf('_unpurified') == -1 && remaining.push(val);
-        //     });
-
-        //     console.log('remaining:', remaining);
-
-        //     if (remaining.length > 0) {
-        //         this.save();
-        //     }
-        // }
     });
 });
