d1871edebd0372739fb4b426c9d26abd7251cb1b
Kees Kluskens <kees@webduck.nl>
2016-06-18 16:49:12 +0200

    Remove useless comments
    These comments explained what happened instead of why. That's completely useless.

--- src/helper/sync.js
+++ src/helper/sync.js
@@ -1,29 +1,14 @@
-// # Sync helper
-//
-// ### _Sync helper to add extra events to Backbone.sync._
-//
-// ___
-//
-// **Author:** AB Zainuddin
-//
-// **Email:** burhan@codeyellow.nl
-//
-// **Website:** http://www.codeyellow.nl
-//
-// **Copyright:** Copyright (c) 2013 Code Yellow B.V.
-//
-// **License:** Distributed under MIT license.
-// ___
+/**
+ * Sync helper to add extra events to Backbone.sync
+ */
 export default {
-    // Add events to sync.
     events(parent) {
         return function (method, model, options) {
             let xhr = null;
 
-            // Shorthand function for triggering events.
             const trigger = function (triggerStr, ...args) {
                 if (model.trigger) {
-                    if (triggerStr.match(/success/)) { // DIRTY HACK - Pass along data from XHR if it was successful *RickDG
+                    if (triggerStr.match(/success/)) {
                         model.trigger(triggerStr, args[0]);
                     } else {
                         model.trigger(triggerStr);
@@ -34,13 +19,10 @@ export default {
             // Name of the flag to keep track of requests.
             const flag = `inSync${method.charAt(0).toUpperCase()}${method.slice(1)}`;
 
-            // Set flag.
             model[flag] = true;
 
-            // Trigger 'before' event.
             trigger(`before:${method}`);
 
-            // Call parent.
             xhr = parent.call(this, method, model, options);
 
             // Trigger 'after' event. If xhr exists, then request is in progress.
@@ -61,7 +43,6 @@ export default {
                     trigger(`after:${method}`);
                 });
             } else {
-                // Set flag.
                 model[flag] = false;
                 trigger(`after:${method}`);
             }
