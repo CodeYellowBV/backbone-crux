b3b8c98bb576669af3fc3c9ebc2d6e9d0c46c98a
Kees Kluskens <kees@webduck.nl>
2016-08-14 14:32:53 +0200

    Add tests for Marionette.plugins with behaviors

--- test/plugins.js
+++ test/plugins.js
@@ -2,6 +2,7 @@ import test from 'ava';
 import _ from 'underscore';
 import Marionette from 'backbone.marionette';
 import '../dist/backbone-crux';
+import sinon from 'sinon';
 
 test.beforeEach(t => {
     t.context.app = new Marionette.Application();
@@ -30,11 +31,16 @@ function testView(t, viewType) {
         },
     });
     const view = new View();
+    const bindSpy = sinon.spy(view.plugins.mask, 'bind');
+    const unbindSpy = sinon.spy(view.plugins.mask, 'unbind');
 
     t.context.app.content.show(view);
+    t.true(bindSpy.calledOnce);
+    t.false(unbindSpy.called);
     t.is(view.$el.text(), 'Bind!');
 
     t.context.app.content.empty();
+    t.true(bindSpy.calledOnce);
     t.is(view.$el.text(), 'Unbind!');
 }
 
@@ -79,6 +85,39 @@ test('multiple plugin bind / unbind should work', t => {
     t.is(view.$el.text(), 'UnbindA!UnbindB!');
 });
 
+test('should work with behaviors', t => {
+    const Foo = Marionette.Behavior.extend({
+        ui: {
+            eureka: '.eureka',
+        },
+        plugins: {
+            mask: {
+                bind() {
+                    this.ui.eureka.text('Behavior!');
+                },
+                unbind() {
+                    this.ui.eureka.text('Unbehavior!');
+                },
+            },
+        },
+    });
+    const View = Marionette.ItemView.extend({
+        template: _.template('<div class="eureka"></div>'),
+        behaviors: {
+            foo: {
+                behaviorClass: Foo,
+            },
+        },
+    });
+    const view = new View();
+
+    t.context.app.content.show(view);
+    t.is(view.$el.text(), 'Behavior!');
+
+    t.context.app.content.empty();
+    t.is(view.$el.text(), 'Unbehavior!');
+});
+
 
 test('defining plugins as a function', t => {
     const View = Marionette.ItemView.extend({
@@ -97,10 +136,13 @@ test('defining plugins as a function', t => {
         },
     });
     const view = new View();
+    const spy = sinon.spy(view, 'plugins');
 
     t.context.app.content.show(view);
+    t.true(spy.calledOnce);
     t.is(view.$el.text(), 'BindA!');
 
     t.context.app.content.empty();
+    t.true(spy.calledOnce);
     t.is(view.$el.text(), 'UnbindA!');
 });
